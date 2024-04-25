8fb4f45b97acd8fa61457531c0b755185c4c56cd
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2024-04-25 22:26:18 +0300

    feat: add aside navigation panel

---
+++ startup-summer-24/src/components/AsideNavigation.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import { Box, List } from "@mantine/core";
+import "../styles/AsideNavigation.css";
+
+export const AsideNavigation = () => {
+  const setActiveLink = ({ isActive }) => isActive ? "navigation-link navigation-link__active" : "navigation-link";
+
+  return (
+    <Box component="nav">
+      <List listStyleType="none" >
+        <List.Item w="100%" mb={16} className="navigation-list-item">
+          <NavLink to="/" className={setActiveLink}>Movies</NavLink>
+        </List.Item>
+        <List.Item w="100%" mb={16} className="navigation-list-item">
+          <NavLink to="/rated" className={setActiveLink}>Rated movies</NavLink>
+        </List.Item>
+      </List>
+    </Box>
+  )
+}
