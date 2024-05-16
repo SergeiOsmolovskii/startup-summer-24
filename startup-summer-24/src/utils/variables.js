214796c8e0935370d6db3d11fae5f6ef987ca661
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2024-05-16 23:11:04 +0300

    feat: add filters change filters logic

---
+++ startup-summer-24/src/utils/variables.js
@@ -0,0 +1,50 @@
+export const SORT_PARAMS = [
+  {
+    value: "popularity.asc",
+    label: "Least Popular"
+  },
+  {
+    value: "popularity.desc",
+    label: "Most Popular"
+  },
+  {
+    value: "revenue.asc",
+    label: "Least Revenue"
+  },
+  {
+    value: "revenue.desc",
+    label: "Most Revenue"
+  },
+  {
+    value: "primary_release_date.asc",
+    label: "Oldest Release Date"
+  },
+  {
+    value: "primary_release_date.desc",
+    label: "Newest Release Date"
+  },
+  {
+    value: "vote_average.asc",
+    label: "Lowest Vote Average"
+  },
+  {
+    value: "vote_average.desc",
+    label: "Highest Vote Average"
+  },
+  {
+    value: "vote_count.asc",
+    label: "Least Voted"
+  },
+  {
+    value: "vote_count.desc",
+    label: "Most Voted"
+  },
+  {
+    value: "title.asc",
+    label: "Title (A-Z)"
+  },
+  {
+    value: "title.desc",
+    label: "Title (Z-A)"
+  }
+];
